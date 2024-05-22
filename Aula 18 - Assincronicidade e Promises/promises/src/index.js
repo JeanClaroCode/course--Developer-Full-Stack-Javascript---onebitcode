cd4393593b18a3baf4121b7c9c9a509edba20082
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-05-22 11:19:32 -0300

    finish

--- Aula 18 - Assincronicidade e Promises/promises/src/index.js
+++ Aula 18 - Assincronicidade e Promises/promises/src/index.js
@@ -1,12 +1,12 @@
 let komodoShip = {
     name: "Komodo",
     velocity: 80,
-    acceleration: 10
+    acceleration: 0
 }
 
 const velocityAfter2Seconds = (velocity, acceleration) => {
 
-    new Promise(function(resolve, reject){
+    return new Promise(function(resolve, reject){
         setTimeout(() => {
         if(acceleration > 0){
             velocity += acceleration*2
@@ -16,10 +16,16 @@ const velocityAfter2Seconds = (velocity, acceleration) => {
             console.log("Aceleração inválida")
             reject("Não possui aceleração")
         }
-    }, 2000);
+    }, 1000);
     })
 }
 
-velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration)
+velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration).then(velocity => {
+    komodoShip.velocity = velocity
+    console.log("Depois de acelerar:\n", komodoShip)
+}).catch(message => {
+    console.log(`Komodo: ${message}`)
+})
 
 console.log("Execução de Promises")
+console.log(komodoShip)
