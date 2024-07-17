1ce304ae27d291182c59eb916938b6d8b68b65ae
Jean Claro <jeanclarodev@gmail.com>
2024-07-16 21:23:03 -0300

    Delete Projeto Final/production directory
    Fim

--- Projeto Final/production/javascript_note_api/config/database.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-
-mongoose.connect('mongodb://localhost/javascriptNote', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log('Connection successful'))
-.catch((err) => console.log(err));
